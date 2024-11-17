644f2fee231daa5bb4149ae7cfc2dac4c547b260
Takahiro Yasui <hikoalpha@gmail.com>
2024-11-17 18:16:55 +0900

    URP Asset EditorにDestroyImmediate追加

--- Assets/Scripts/URPAssetSetting.cs
+++ Assets/Scripts/URPAssetSetting.cs
@@ -11,11 +11,16 @@ public class URPAssetSetting : MonoBehaviour {
 
 #if UNITY_EDITOR
 [CustomEditor(typeof(URPAssetSetting))]
-public class URPAssetSettingEditor : Editor
-{
+public class URPAssetSettingEditor : Editor {
+    private URPAssetSetting urpAssetSetting;
+    // private Editor urpDataEditor;
+    
+    private void OnEnable() {
+        urpAssetSetting = (URPAssetSetting)target;
+        // urpDataEditor = null;
+    }
+
     public override void OnInspectorGUI() {
-        URPAssetSetting urpAssetSetting = (URPAssetSetting)target;
-        
         EditorGUILayout.BeginHorizontal();
         
         urpAssetSetting.urpAsset = (UniversalRenderPipelineAsset)EditorGUILayout.ObjectField(
@@ -42,19 +47,60 @@ public class URPAssetSettingEditor : Editor
             DestroyImmediate(editorAsset);
         }
         
-        GUILayout.Space(10);
-        GUILayout.Box("", GUILayout.Height(2), GUILayout.ExpandWidth(true));
-        EditorGUILayout.LabelField("URP Data", EditorStyles.boldLabel);
-        EditorGUI.indentLevel++;
         
-        var editorData = CreateEditor(urpAssetSetting.urpData);
-        if (editorData != null) {
-            editorData.OnInspectorGUI();
-            DestroyImmediate(editorData);
+        // UniversalRendererDataEditorでCreateEditorするとUnityエディタのバグでEditorインスタンスが削除できなくなり
+        // 実行時にSerializedObjectNotCreatableException: Object at index 0 is nullエラーが発生するため使用できない
+        // DestroyImmediate()でインスタンスを削除するとエラーは回避できるがRendererFeatureListがクリアされる
+        // https://discussions.unity.com/t/im-getting-now-error-serializedobjectnotcreatableexception-object-at-index-0-is-null-how-to-fix/788961
+        
+        // GUILayout.Space(10);
+        // EditorGUILayout.LabelField("URP Data", EditorStyles.boldLabel);
+        // EditorGUI.indentLevel++;
+        //
+        // if (urpAssetSetting.urpData != null) {
+        //     urpDataEditor = CreateEditor(urpAssetSetting.urpData);
+        //     urpDataEditor.OnInspectorGUI();
+        // }
+        
+        if (GUILayout.Button("Show URP Data")) {
+            URPDataWindow.Open(CreateEditor(urpAssetSetting.urpData));
         }
+    }
+
+    private void OnValidate() {
+        Debug.Log("OnValidate");
+    }
+
+    private void OnDestroy() {
+        Debug.Log("OnDestroy");
+    }
+
+    private void OnDisable() {
+        Debug.Log("OnDisable");
     }
 }
 
+internal class URPDataWindow : EditorWindow {
+    
+    private Editor urpDataEditor;
+    
+    public static void Open(Editor urpDataEditor)
+    {
+        var window = GetWindow<URPDataWindow>("URP Data");
+        window.urpDataEditor = urpDataEditor;
+    }
+
+    private void OnGUI()
+    {
+        EditorGUI.indentLevel++;
+        EditorGUIUtility.labelWidth = 250;
+        urpDataEditor.OnInspectorGUI();
+    }
+
+    private void OnDestroy() {
+        DestroyImmediate(urpDataEditor);
+    }
+}
 
 public static class UniversalRenderPipelineAssetExtensions
 {
